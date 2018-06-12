bedcbb1981cb232b3e6b990607395d538a94a592
shejian <shejian@taiwu.com>
2018-06-12 14:36:03 +0800

    新增函数

--- src/main/java/com/avengers/mylibrary/CommonUtil.java
+++ src/main/java/com/avengers/mylibrary/CommonUtil.java
@@ -5,12 +5,18 @@ import android.util.Log;
 public class CommonUtil {
 
     public static void print() {
-        Log.d("shejian", "I'm eathmodule");
+        Log.d("shejian", "I'm eathmodule 121231231231231231232");
     }
 
     public static int plus(int num1, int num2) {
 
         return num1 + num2;
     }
+
+    public static int plus2(int num1, int num2) {
+
+        return num1 + num2;
+    }
+
 
 }
