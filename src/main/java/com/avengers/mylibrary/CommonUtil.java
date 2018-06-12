2b47205c01b744170b947e80f6fc7ea03f1e5e97
shejian <shejian@taiwu.com>
2018-06-12 14:31:02 +0800

    初始化子项目

--- src/main/java/com/avengers/mylibrary/CommonUtil.java
+++ src/main/java/com/avengers/mylibrary/CommonUtil.java
@@ -4,8 +4,13 @@ import android.util.Log;
 
 public class CommonUtil {
 
-    public static void print(){
-        Log.d("shejian","I'm eathmodule");
+    public static void print() {
+        Log.d("shejian", "I'm eathmodule");
+    }
+
+    public static int plus(int num1, int num2) {
+
+        return num1 + num2;
     }
 
 }
