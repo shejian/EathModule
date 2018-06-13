aabfc8d106b253ded4bf5b64a54301700fcae781
shejian <shejian@taiwu.com>
2018-06-13 16:49:36 +0800

    gradle 的配置调整

--- src/main/java/com/avengers/mylibrary/CommonUtil.java
+++ src/main/java/com/avengers/mylibrary/CommonUtil.java
@@ -1,10 +1,16 @@
 package com.avengers.mylibrary;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ImageView;
 
 public class CommonUtil {
 
-    public static void print() {
+    public void print() {
 
         Log.d("shejian", "I'm eathmodule 000000");
 
@@ -12,15 +18,44 @@ public class CommonUtil {
 
     }
 
-    public static int plus(int num1, int num2) {
+    public int plus(int num1, int num2) {
 
         return num1 + num2;
     }
 
-    public static int plus2(int num1, int num2) {
+    public int plus2(int num1, int num2) {
 
         return num1 + num2;
     }
 
+    public Drawable loadImg(String url) {
+
+        return new Drawable() {
+            @Override
+            public void draw(@NonNull Canvas canvas) {
+
+            }
+
+            @Override
+            public void setAlpha(int alpha) {
+
+            }
+
+            @Override
+            public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+            }
+
+            @Override
+            public int getOpacity() {
+                return 0;
+            }
+        };
+    }
+
+    public void showImg(ImageView view, Drawable drawable) {
+
+        Log.d("shejian", "加载完成");
+    }
 
 }
